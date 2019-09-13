5d401082d1b45ee5ae970bc40c114fed7e9cb1b5
JosE-rnesto <52289554+JosE-rnesto@users.noreply.github.com>
2019-09-13 09:37:57 -0600

    Agregar archivos de proyecto.

---
+++ AspTallerMecanico/Data/AspTallerMecanicoContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AspTallerMecanico.Data.DDL;
+
+namespace AspTallerMecanico.Models
+{
+    public class AspTallerMecanicoContext : DbContext
+    {
+        public AspTallerMecanicoContext (DbContextOptions<AspTallerMecanicoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AspTallerMecanico.Data.DDL.Clientes> Clientes { get; set; }
+
+        public DbSet<AspTallerMecanico.Data.DDL.Vehiculo> Vehiculo { get; set; }
+
+        public DbSet<AspTallerMecanico.Data.DDL.TipoVehiculo> TipoVehiculo { get; set; }
+
+        public DbSet<AspTallerMecanico.Data.DDL.TipoServicio> TipoServicio { get; set; }
+
+        public DbSet<AspTallerMecanico.Data.DDL.ServicioMecanico> ServicioMecanico { get; set; }
+    }
+}
